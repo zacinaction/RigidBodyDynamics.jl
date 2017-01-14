3ba6691d578877428ee72e2b38aeba1bc08e8805
Twan Koolen <koolen.twan@gmail.com>
2017-01-14 09:38:46 -0500

    simplify

--- segfault.jl
+++ segfault.jl
@@ -3,38 +3,29 @@ using StaticArrays
 
 typealias ContiguousSMatrixColumnView{S1, S2, T, L} SubArray{T,2,SMatrix{S1, S2, T, L},Tuple{Colon,UnitRange{Int64}},true}
 
-
-function smatrix3x6view{T}(mat::SMatrix{3, 0, T, 0})
-    data = fill(NaN, SMatrix{3, 6, T, 18})
-    ret = view(data, :, 1 : 0)
-end
-
 immutable WrenchMatrix{A<:AbstractMatrix}
     angular::A
-    linear::A
-
-    function WrenchMatrix(angular::A, linear::A)
-        new(angular, linear)
-    end
-end
-
-function WrenchMatrix{A<:AbstractMatrix}(angular::A, linear::A)
-    WrenchMatrix{A}(angular, linear)
 end
 
 typealias WrenchSubspace{T} WrenchMatrix{ContiguousSMatrixColumnView{3, 6, T, 18}}
 
 abstract JointType{T<:Real}
-immutable QuaternionFloating{T} <: JointType{T}
-end
-function _constraint_wrench_subspace{T}(::QuaternionFloating{T})
-    angular = zeros(SMatrix{3, 0, T})
-    linear = zeros(SMatrix{3, 0, T})
-    WrenchMatrix(smatrix3x6view(angular), smatrix3x6view(linear))
-end
+
+immutable QuaternionFloating{T} <: JointType{T} end
 
 type Joint{T<:Real}
     jointType::JointType{T}
+end
+
+function smatrix3x6view(mat::SMatrix{3, 0}) # mat must not have concrete type
+    T = eltype(mat)
+    data = fill(NaN, SMatrix{3, 6, T})
+    ret = view(data, :, 1 : 0)
+end
+
+function _constraint_wrench_subspace{T}(::QuaternionFloating{T})
+    angular = zeros(SMatrix{3, 0, T})
+    WrenchMatrix(smatrix3x6view(angular))
 end
 
 function constraint_wrench_subspace{M}(joint::Joint{M})::WrenchSubspace{M}
@@ -43,8 +34,8 @@ end
 
 end # module
 
-js = (Segfault.Joint(Segfault.QuaternionFloating{Float64}()),)
-for joint in js
+joints = (Segfault.Joint(Segfault.QuaternionFloating{Float64}()),)
+for joint in joints
     T = Segfault.constraint_wrench_subspace(joint)
     println(length(T.angular))
 end
