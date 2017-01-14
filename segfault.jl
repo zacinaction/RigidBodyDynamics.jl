9dfa5f0c7ac93e79b352a474cdbdd90db6db5b50
Twan Koolen <koolen.twan@gmail.com>
2017-01-14 11:47:43 -0500

    simplify

--- segfault.jl
+++ segfault.jl
@@ -2,38 +2,25 @@ module Segfault
 using StaticArrays
 
 # typealias for typeof(view(rand(SMatrix{S1, S2, T, L}), :, 1:0)):
-typealias ViewType{S1, S2, T, L} SubArray{T,2,SMatrix{S1, S2, T, L},Tuple{Colon,UnitRange{Int64}},true}
+typealias ViewType{S1, S2, T, L} SubArray{T,2,SMatrix{S1, S2, T, L},Tuple{Colon,UnitRange{Int64}},true} # for 0.5
+# typealias ViewType{S1, S2, T, L} SubArray{T,2,SMatrix{S1,S2,T,L},Tuple{Base.Slice{Base.OneTo{Int64}},UnitRange{Int64}},true} # for latest master
 
-immutable ArrayHolder{A<:AbstractMatrix}
+immutable MatrixHolder{A<:AbstractMatrix}
     a::A
 end
 
-typealias SpecificArrayHolder{T} ArrayHolder{ViewType{3, 6, T, 18}}
+typealias SpecificMatrixHolder{T} MatrixHolder{ViewType{3, 6, T, 18}}
 
-immutable Foo{T} end
+type Foo{T} end
 
-type FooHolder{T<:Real}
-    foo::Foo{T}
-end
-
-function smatrix3x6view(mat::SMatrix{3, 1}) # mat's type must not be concrete
-    T = eltype(mat)
+function bar{T}(fooHolder::Foo{T})::SpecificMatrixHolder{T} # return type annotation required
     data = fill(NaN, SMatrix{3, 6, T})
-    ret = view(data, :, 1 : 0)
-end
-
-function _bar{T}(::Foo{T})
-    a = zeros(SMatrix{3, 1, T})
-    ArrayHolder(smatrix3x6view(a))
-end
-
-function bar{M}(fooHolder::FooHolder{M})::SpecificArrayHolder{M} # return type annotation required
-    _bar(fooHolder.foo)
+    MatrixHolder(view(data, :, 1 : 0))
 end
 
 end # module
 
-fooHolders = (Segfault.FooHolder(Segfault.Foo{Float64}()),)
+fooHolders = (Segfault.Foo{Float64}(),)
 for fooHolder in fooHolders
     arrayHolder = Segfault.bar(fooHolder) # call to bar must occur in for loop
     println(length(arrayHolder.a))
